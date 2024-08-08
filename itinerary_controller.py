bfb83778ca881ef62267d0b894c43bb4dc242699
xXFrostXx <48761453+xXFrostXx1@users.noreply.github.com>
2024-08-08 16:17:19 +0200

    added function

--- itinerary_controller.py
+++ itinerary_controller.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import and_
 from os import environ
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
@@ -25,8 +27,8 @@ def create_itinerary():
     data = request.get_json()
 
     new_itinerary = Itinerary(destination=data['destination'],
-                              start_date=data['start_date'],
-                              end_date=data['end_date'])
+                              start_date=datetime.strptime(data['start_date'], "%Y-%m-%d").date(),
+                              end_date=datetime.strptime(data['end_date'], "%Y-%m-%d").date())
     db.session.add(new_itinerary)
     db.session.commit()
 
@@ -34,15 +36,28 @@ def create_itinerary():
 
 @app.route('/itineraries', methods=['GET'])
 def get_itineraries():
-    itineraries = Itinerary.query.all()
+    query_params = request.args
+    destination_query = query_params.get('destination')
+    start_date_query = query_params.get('start_date')
+    end_date_query = query_params.get('end_date')
+    
+    query = Itinerary.query
+    
+    if destination_query:
+        query = query.filter(Itinerary.destination.ilike(f'%{destination_query}%'))
+    if start_date_query and end_date_query:
+        query = query.filter(and_(Itinerary.start_date >= datetime.strptime(start_date_query, "%Y-%m-%d").date(), Itinerary.end_date <= datetime.strptime(end_date_query, "%Y-%m-%d").date()))
+    
+    itineraries = query.all()
+    
     return jsonify([{"id": itinerary.id, "destination": itinerary.destination,
-                     "start_date": itinerary.start_date, "end_date": itinerary.end_date} for itinerary in itineraries])
+                     "start_date": str(itinerary.start_date), "end_date": str(itinerary.end_date)} for itinerary in itineraries])
 
 @app.route('/itineraries/<int:id>', methods=['GET'])
 def get_itinerary(id):
     itinerary = Itinerary.query.get_or_404(id)
     return jsonify({"id": itinerary.id, "destination": itinerary.destination,
-                    "start_date": itinerary.start_date, "end_date": itinerary.end_date})
+                    "start_date": str(itinerary.start_date), "end_date": str(itinerary.end_date)})
 
 @app.route('/itineraries/<int:id>', methods=['PUT'])
 def update_itinerary(id):
@@ -50,8 +65,8 @@ def update_itinerary(id):
     data = request.get_json()
 
     itinerary.destination = data['destination']
-    itinerary.start_date = data['start_date']
-    itinerary.end_date = data['end_date']
+    itinerary.start_date = datetime.strptime(data['start_date'], "%Y-%m-%d").date()
+    itinerary.end_date = datetime.strptime(data['end_date'], "%Y-%m-%d").date()
 
     db.session.commit()
 
